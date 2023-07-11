8966c63a43bf435be61e3c4063d9b315595c1d14
sami-ul-haq <samiulhaq1810@gmail.com>
2023-07-11 14:44:10 +0500

    blogs added

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,13 +1,30 @@
 const getPosts = async () => {
-  const response = await fetch();
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   return await response.json();
-}
+};
 
+const Blog = async () => {
+  const posts = await getPosts();
 
-const Blog = () => {
   return (
-    <div>This is blog page</div>
-  )
-}
+    <div>
+      <h2 className="text-[32px] font-bold">Posts</h2>
+      <p className="text-[#888] text-xl">
+        All the tools you need to make the Web. Faster.
+      </p>
+      <div className="mt-10 content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            className="post-box rounded-md border border-[#333] p-4 space-y-2 hover:bg-[#333]"
+          >
+            <h1>{post.title}</h1>
+            <p> {post.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
